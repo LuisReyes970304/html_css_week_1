86c6a22acbd18554e79ca9658fe2106691601a03
Luis Reyes <luisrafaelreyescaro1@gmail.com>
2026-04-20 00:18:46 -0500

    dark mode added

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 const mainNav = document.querySelector('.main_nav');
-const dropdown = document.querySelector('.dropdown');
+const dropdown = document.querySelectorAll('.dropdown');
 const header_btn = mainNav.querySelectorAll('button');
 
 header_btn.forEach(btn => {
-    btn.addEventListener('click', () => {
-        dropdown.classList.toggle('show');
+    btn.addEventListener('click', (e) => {
+        e.stopPropagation();
+        dropdown.forEach(down => {
+            if (down.id === btn.value) {
+                    down.classList.toggle("show");
+                };
+            });
+        });
     });
+
+dropdown.forEach(down => {
+    down.addEventListener("click", (e) => {
+        e.stopPropagation();
+    });
+});
+
+window.addEventListener("click", () => {
+    dropdown.forEach(down => {
+        down.classList.remove("show");
+    });
+});
+
+const darkMode = document.getElementById("dark_mode");
+const root = document.querySelector(":root"); 
+
+darkMode.addEventListener("click", () => {
+    root.classList.toggle("dark_mode");
+    darkMode.disabled = true;
+    classic.disabled = false;
+    darkMode.style.color = "grey";
+    classic.style.color = "white";
+});
+
+const classic = document.getElementById("classic");
+
+classic.addEventListener("click", () => {
+    root.classList.toggle("dark_mode");
+    classic.disabled = true;
+    darkMode.disabled = false;
+    classic.style.color = "grey";
+    darkMode.style.color = "white";
 });
